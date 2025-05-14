ee00080b2e92de67cd4aaab69e3a74f476f0b3b8
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2025-05-15 05:08:46 +0900

    build setup

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,18 @@ const is_prod = process.env.NODE_ENV === "production";
 /**
  * Return prerendering pages
  */
-const getPrerenderEntries = async () => {
+function getPrerenderEntries() {
   let list = ["*"];
   if (is_prod) {
-    const routes = await JSON.parse(
-      fs.readFileSync("contents/article_routes.json", "utf-8"),
-    );
-    list = list.concat(routes);
+    const data = fs.readFileSync("contents/article_routes.json", "utf-8");
+    const routes = JSON.parse(data);
+
+    // routes は '/blog/' (paths.base) を含むルートを返す。
+    // しかし prerender.entries には paths.base を含まないルートを渡す必要がある。
+    // そのためここで `/blog/` を削除したルートに変換する。
+    list = list.concat(routes.map((route) =>
+      route.startsWith("/blog/") ? route.slice(5) : route
+    ));
   }
   return list;
 };
@@ -34,7 +39,7 @@ const config = {
     },
     adapter: adapter(),
     prerender: {
-      entries: await getPrerenderEntries(),
+      entries: getPrerenderEntries(),
     },
   },
 };
